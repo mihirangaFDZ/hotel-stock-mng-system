49cbcba3ef8075f3db5387545235ef39041e03d5
Mihiranga Fernando <it22585394@my.sliit.lk>
2025-03-13 00:37:13 +0530

    Updated files after removing lines

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
-import Inventory from './components/Inventory';
-import ShoppingList from './components/ShoppingList';
-import UserManagement from './components/UserManagement';
-import WasteTracking from './components/WasteTracking';
-import AIBot from './components/AIBot';
+
 
 function App() {
   return (
@@ -16,11 +12,6 @@ function App() {
         <div className="max-w-7xl mx-auto px-4 py-6">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            <Route path="/inventory" element={<Inventory />} />
-            <Route path="/shopping-list" element={<ShoppingList />} />
-            <Route path="/users" element={<UserManagement />} />
-            <Route path="/waste-tracking" element={<WasteTracking />} />
-            <Route path="/ai-bot" element={<AIBot />} />
           </Routes>
         </div>
       </div>
